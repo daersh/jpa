8a418f9d36796e9d1f3273272eb1143d1404b970
daersh <daersh@naver.com>
2024-02-26 14:06:22 +0900

    chap02 auto ddl 확인용 예제

---
+++ chap02-mapping-lecture-source/src/test/java/com/ohgiraffers/section01/entity/EntityMappingTests.java
@@ -0,0 +1,66 @@
+package com.ohgiraffers.section01.entity;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EntityMappingTests {
+    private static EntityManagerFactory entityManagerFactory;
+    private EntityManager entityManager;
+
+    @BeforeAll
+    public static void initFactory(){
+        /*1. 환경 생성*/
+        entityManagerFactory = Persistence.createEntityManagerFactory("jpatest");
+    }
+    @BeforeEach
+    public void initManager(){
+        /*2. manager 연결*/
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @AfterAll
+    public static void closeFactory(){
+        entityManagerFactory.close();
+    }
+    @AfterEach
+    public void closeManager(){
+        entityManager.close();  // 설명. close 수행 시 flush, commit이 일어나 실제 디비에 영향을 주게 된다.
+    }
+
+    @Test
+    public void 테이블_만들기_test(){
+        //given
+        Member member = new Member();
+        member.setMemberNo(1);
+        member.setMemberId("user01");
+        member.setMemberPwd("pass01");
+        member.setNickname("홍길동");
+        member.setPhone("010-1234-5678");
+        member.setEmail("hong@gmail.com");
+        member.setAddress("서울시 서초구");
+        member.setEnrollDate(new java.util.Date());
+        member.setMemberRole("ROLE_MEMBER");
+        member.setStatus("Y");
+
+
+        //when
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+        entityManager.persist(member);
+
+        //then
+        Member foundMember = entityManager.find(Member.class,1);/*테이블, 값 넣어줌*/
+        foundMember.setNickname("동해번쩍서에번쩍");
+
+        entityTransaction.commit();
+        assertEquals(member,foundMember);
+        /*값을 업데이트되어 있는 것을 알 수 있다. */
+        System.out.println("foundMember = " + foundMember);
+    }
+
+}
